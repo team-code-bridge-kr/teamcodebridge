ebc60b14cf38a53b772687c97fd58c6a94ccf582
jangwonjun <won2005won@naver.com>
2025-12-28 14:25:46 +0900

    [Add] 파비콘 적용 및 메타데이터 업데이트

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'TeamCodeBridge | 가능성의 끝에서 IT 교육의 시작!',
   description: '연합 봉사 동아리로, 멘토링·IT공모전·해커톤 경험을 바탕으로 초/중/고등학생 대상 AI·코딩 기초부터 실습, 미니 해커톤까지 체계적 멘토링과 진로 탐색 지원을 제공합니다.',
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({
